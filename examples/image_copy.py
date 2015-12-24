6ebeea8d1d32de73a193b7b1ec6f30e817c40de0
Joe Server <joe@jserver.io>
2015-12-24 15:19:52 -0500

    fix type and remove old README message

--- examples/image_copy.py
+++ examples/image_copy.py
@@ -8,7 +8,7 @@ home = os.environ['HOME']
 OrdinaryCallingFormat = boto.config.get('s3', 'calling_format', 'boto.s3.connection.OrdinaryCallingFormat')
 
 s3 = boto.connect_s3(host='localhost', port=10001, calling_format=OrdinaryCallingFormat, is_secure=False)
-b = s3.get_bucket('mockmng')
+b = s3.get_bucket('mocking')
 
 dst_bucket = s3.create_bucket('backup')
 
